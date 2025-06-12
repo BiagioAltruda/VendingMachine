43883017fde802805b3777acc02972a67fb78459
Alessio <zhangalessio452@gmail.com>
2025-06-12 10:35:24 +0200

    nigger

--- src/Beverages.java
+++ src/Beverages.java
@@ -3,7 +3,8 @@ public class Beverages {
 	private String productName;
 	private int productCode;
 	private double productPrice;
-	private int quantity;
-	
+    private int quantity;
+    
+//niggers
 	
 }
